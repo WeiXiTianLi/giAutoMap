31773b245ae3b1402736075c5aa4ee9db30b4538
GengGode <GengGode@outlook.com>
2021-07-21 19:49:48 +0800

    去除无用类

--- GenshinImpact_AutoMap/ATM_ThreadMatch.cpp
+++ GenshinImpact_AutoMap/ATM_ThreadMatch.cpp
@@ -921,65 +921,65 @@ double ATM_TM_ORBAvatar::Line2Angle(Point2f p)
 	return res;
 }
 
-void ATM_TM_Thread::run()
-{
-	while (isExitThread == false)
-	{
-		if (isRunWork && (*ptr) != nullptr)
-		{
-			ptr(workInput);
-			isRunWork = false;
-			isEndWork = true;
-		}
-		else
-		{
-			std::this_thread::sleep_for(std::chrono::milliseconds(1));
-		}
-	}
-}
-
-ATM_TM_Thread::ATM_TM_Thread()
-{
-	tLoopWork = new thread(&ATM_TM_Thread::run, this);
-}
-
-ATM_TM_Thread::~ATM_TM_Thread()
-{
-	if (tLoopWork != nullptr)
-	{
-		isExitThread = true;
-		tLoopWork->join();
-		delete tLoopWork;
-	}
-}
-
-ATM_TM_Thread::ATM_TM_Thread(void(*funPtr)(Mat &inMat))
-{
-	setFunction(funPtr);
-	tLoopWork = new thread(&ATM_TM_Thread::run, this);
-}
-
-void ATM_TM_Thread::setFunction(void(*funPtr)(Mat &inMat))
-{
-	ptr = funPtr;
-	isExistFunction = true;
-}
-
-void ATM_TM_Thread::start(Mat & inMat)
-{
-	if (isExistFunction == false)
-	{
-		throw"Not Found Work Function";
-	}
-	workInput = inMat;
-	isRunWork = true;
-	isEndWork = false;
-}
-
-bool ATM_TM_Thread::isEnd()
-{
-	return isEndWork;
-}
+//void ATM_TM_Thread::run()
+//{
+//	while (isExitThread == false)
+//	{
+//		if (isRunWork && (*ptr) != nullptr)
+//		{
+//			ptr(workInput);
+//			isRunWork = false;
+//			isEndWork = true;
+//		}
+//		else
+//		{
+//			std::this_thread::sleep_for(std::chrono::milliseconds(1));
+//		}
+//	}
+//}
+//
+//ATM_TM_Thread::ATM_TM_Thread()
+//{
+//	tLoopWork = new thread(&ATM_TM_Thread::run, this);
+//}
+//
+//ATM_TM_Thread::~ATM_TM_Thread()
+//{
+//	if (tLoopWork != nullptr)
+//	{
+//		isExitThread = true;
+//		tLoopWork->join();
+//		delete tLoopWork;
+//	}
+//}
+//
+//ATM_TM_Thread::ATM_TM_Thread(void(*funPtr)(Mat &inMat))
+//{
+//	setFunction(funPtr);
+//	tLoopWork = new thread(&ATM_TM_Thread::run, this);
+//}
+//
+//void ATM_TM_Thread::setFunction(void(*funPtr)(Mat &inMat))
+//{
+//	ptr = funPtr;
+//	isExistFunction = true;
+//}
+//
+//void ATM_TM_Thread::start(Mat & inMat)
+//{
+//	if (isExistFunction == false)
+//	{
+//		throw"Not Found Work Function";
+//	}
+//	workInput = inMat;
+//	isRunWork = true;
+//	isEndWork = false;
+//}
+//
+//bool ATM_TM_Thread::isEnd()
+//{
+//	return isEndWork;
+//}
 
 int ATM_TM_TemplateUID::getMaxID(double lis[], int len)
 {
